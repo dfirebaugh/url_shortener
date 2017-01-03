92ee5f23cda1e66e4d8606a7068c0fdfa33194cb
Dustin Firebaugh <dafirebaugh@gmail.com>
2017-01-03 21:54:07 +0000

    added unique id to document -- added redirect

--- server.js
+++ server.js
@@ -26,10 +26,26 @@ var redirect = function(collection){
         //make a connection to the document
         //find the document that has the same tinyurl
         //redirect to the original url
+        collection.find().toArray(function(err, items, resObj){
+                if (err) throw err 
+                var match, matchId
+                var smallUrl = "https://" + req.host +"/new/"+ req.params.url
+                console.log("Small url is "+smallUrl)
+                
+                for(var x = 0; x< items.length;x++){
+                    if(items[x].tinyurl == smallUrl){
+                        console.log(items[x].tinyurl)
+                        match = true
+                        matchId = items[x].id
+                        }
+                    }
+                    console.log(items[matchId].originalurl)
+                    res.redirect('https://'+items[matchId].originalurl)
+        })
         
         
         //res.redirect('http://cnn.com/');
-        console.log(req.params.url)
+        //console.log(req.params.url)
     })
     
 }
@@ -38,73 +54,84 @@ var createTiny = function(collection){
     app.get('/:query', function (req, res) {
         
         var originalurl = req.params.query
-        var shorturl = "https://"+req.host + "/new/1";
+        var shorturl = "https://"+req.host + "/new/";
 
         console.log(req.params.query)
         
         //if it's not a url -- send error message
         if(validUrl(req.params.query)){
-            var resObj = {
-                "originalurl": originalurl,
-                'shorturl': shorturl,
-            }
+
 
             //if url doesn't exist -- create new entry
             //does the url exist in the database
             var match = false
             
-            collection.find().toArray(function(err, items){
+            collection.find().toArray(function(err, items, resObj){
                 if (err) throw err 
-                
+                var matchId
                 for(var x = 0; x< items.length;x++){
                     if(items[x].originalurl == originalurl){
                         console.log(items[x].tinyurl)
                         match = true
-                        
+                        matchId = items[x].id
+                        }
                     }
-                    
-                }
                 
                 if(match > 0){
                     console.log('it already exists \n')
-                    
-                }
+                    console.log(items[matchId])
+                    var resObj = {
+                        "error": "url already exists",
+                        "originalurl": originalurl,
+                        "tinyurl": items[matchId].tinyurl
+                    }
+
+                    //it exists so find it in the database and return the results
+                    res.send(resObj)
+                    }
                 else{
                     console.log('adding url to database')
                     //add a unique shorturl
+                    var count = 0
+                    
+                    for(var y=0;y< items.length;y++){
+                        count++
+                    }
+                    console.log('count: ' + count)
+                    shorturl += count
+                    console.log(shorturl)
+                    console.log('count is: '+ count)
                     
                     collection.insert(
                         {
                             "originalurl": originalurl,
-                            "tinyurl": shorturl
+                            "tinyurl": shorturl,
+                            "id":count
                             
                         })
-                    
+                        var resObj = {
+                            "originalurl": originalurl,
+                            'shorturl': shorturl
+                            }
+                            
+                        res.send(resObj)
                 }
-                
             })
-            }
+        }
         else{
             var resObj = {
-                'error': 'not a valid url',
+                'error': 'not a valid url -- please retype the url in the proper format.  e.g. http://example.com',
             }
             console.log('not valid url')
         }
         
-        res.send(resObj)
-        //console.log(resObj)
-            
-
-        
-
-  })
+    })
 }
 
 
 app.listen(port, function () {
   console.log('listening on port: '+ port)
 })
-
 
 
 function validUrl(str) {
